e8f56f1c920165dda15ffee38d98ebcbfeeede5a
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-09-10 23:31:07 +0200

    Initial commit

---
+++ ClawOS/ClawOS.c
@@ -0,0 +1,24 @@
+/*
+ * ClawOS.c
+ *
+ * Created: 10.09.2015 21:45:01
+ *  Author: Benedikt
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
